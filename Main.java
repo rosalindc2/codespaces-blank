263aad99462b5b2f1305709b26d6c75ebc23d765
rosalindc2 <rosalindc2@nycstudents.net>
2024-09-11 13:21:50 +0000

    hw2

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 { 
     public static void main (String[] args)
     { 
-        //String s;
         System.out.print("Hello World!");
+        System.out.println("Hello World");
     }
 }
